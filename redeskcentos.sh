eae0a29ddf882ba215789d7c5716e4e5657c2f14
ajmal017 <ajmal.doc@gmail.com>
2013-12-08 07:14:47 -0800

    http://estranghera.com/2013/06/installing-a-remote-desktop-on-centos-vps/
    wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el5.rf.i386.rpm rpm -Uhv rpmforge-release-0.5.3-1.el5.rf.i386.rpm
    
    yum -y update
    
    yum -y groupinstall "X Window System" "GNOME Desktop Environment yum install -y freenx

---
+++ redeskcentos.sh
@@ -0,0 +1,5 @@
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el5.rf.i386.rpm rpm -Uhv rpmforge-release-0.5.3-1.el5.rf.i386.rpm
+
+yum -y update
+
+yum -y groupinstall "X Window System" "GNOME Desktop Environment yum install -y freenx
